f71797d5d7f7abd129c37ecd635b5a1835a20232
Andrei Martins <neo@Andreis-MacBook-Pro.local>
2022-10-17 11:05:05 -0300

    Scala source code test

--- compactor/src/main/scala/main.scala
+++ compactor/src/main/scala/main.scala
@@ -6,8 +6,7 @@ def main(): Unit = {
 
   val buffer = new ListBuffer[(Int, Char)]()
 
-//  var list: List[Char] = List('a', 'a', 'a', 'a', 'x', 'a', 'x', 'x', 'x', 'v', 'y', 'y', 'y', 'y')
-  var list: List[Char] = List('e', 'r', 's', 's', 'x', 'b', 'b', 'b', 'q', 'q', 'w', 'w', 'o', 'p')
+  var list: List[Char] = List('a', 'a', 'a', 'a', 'b', 'c', 'c', 'a', 'a', 'd', 'e', 'e', 'e', 'e')
   list.foreach(char => {
     val value = list.takeWhile(_.equals(char))
     if (value.size != 0) {
